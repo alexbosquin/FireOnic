baa460d3cc054964d0e9be57a652ff82e7f9c325
alexbosquin <alexbosquin@gmail.com>
2018-02-21 20:58:05 -0300

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+//Database
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDHLyGnUQlsR9aGIel_fPlX8gvvuVum3OQ",
+    authDomain: "fireonic-cool.firebaseapp.com",
+    databaseURL: "https://fireonic-cool.firebaseio.com",
+    projectId: "fireonic-cool",
+    storageBucket: "fireonic-cool.appspot.com",
+    messagingSenderId: "392886439911"
+}
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +27,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { ModelTodo, NameTodo } from '../../models/todo';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  todoCollection: AngularFirestoreCollection<ModelTodo>;
+  todo: Observable<ModelTodo[]>
 
+  constructor(public navCtrl: NavController,
+    private angularFirestore: AngularFirestore) {  }
+
+  ionViewDidEnter(){
+    this.todoCollection = this.angularFirestore.collection(NameTodo.collection,
+    ref => ref.orderBy(NameTodo.priority)
+  )
+    this.todo = this.todoCollection.valueChanges();
   }
 
 }
